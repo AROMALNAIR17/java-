efccd7b6da9830baf52233ec01a17c1a55126866
AROMALNAIR17 <aromalaromal024@gmail.com>
2026-01-27 16:49:40 +0530

    Create min stack leetcode.java

---
+++ min stack leetcode.java
@@ -0,0 +1,36 @@
+ 
+
+class MinStack {
+
+    Stack<Integer> stack;
+    int min = Integer.MAX_VALUE;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        if (val <= min) {
+            stack.push(min);    
+            min = val;         
+        }
+        stack.push(val);
+    }
+
+    public void pop() {
+        if (stack.peek() == min) {
+            stack.pop();        
+            min = stack.pop();  
+        } else {
+            stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
